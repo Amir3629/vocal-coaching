6ade8af47a6c1829c67aae31a47715a1449345e8
Amir3629 <amirhossein.firoozi@hotmail.com>
2025-03-05 14:10:20 +0100

    Fixed booking system component

--- app/components/booking-system.tsx
+++ app/components/booking-system.tsx
@@ -2,4 +2,10 @@
 
 import { Card } from "@/components/ui/card"
 
-<Card className="bg-[#0A0A0A] border-[#C8A97E]/20 p-6 md:p-8"> 
+export default function BookingSystem() {
+  return (
+    <Card className="bg-[#0A0A0A] border-[#C8A97E]/20 p-6 md:p-8">
+      {/* Booking system content will go here */}
+    </Card>
+  )
+} 
