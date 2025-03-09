65b88114616a05f36531c476e5d6973969a8b80f
Amir3629 <amirhossein.firoozi@hotmail.com>
2025-03-09 18:01:49 +0100

    Fix translations TypeScript types

--- app/translations.ts
+++ app/translations.ts
@@ -1,1 +1,23 @@
- 
+interface Translation {
+  partners: {
+    title: string;
+  };
+}
+
+interface Translations {
+  EN: Translation;
+  DE: Translation;
+}
+
+export const translations: Translations = {
+  EN: {
+    partners: {
+      title: "Partners & Collaborations"
+    }
+  },
+  DE: {
+    partners: {
+      title: "Partner & Kooperationen"
+    }
+  }
+} 
