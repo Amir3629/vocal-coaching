8a0aaf981e2cfa89d71af46a095e218bb20694a2
Amir3629 <amirhossein.firoozi@hotmail.com>
2025-03-20 20:57:07 +0100

    Add TypeScript and ESLint error ignoring to next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 
 const nextConfig = {
   output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     unoptimized: true,
     remotePatterns: [
